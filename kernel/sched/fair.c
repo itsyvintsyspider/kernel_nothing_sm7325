4f53a24d413544ff65708235a883dd8ca8e87267
Afnaan Shifau <vinproliffy@gmail.com>
2026-08-24 22:10:48 +0500

    sched/bore: nudge penalty tuning for UI latency (fork_atavistic 2->1, offset 18->20, scale 1536->1408)
    Addresses reported jank on QS panel with heavy notifications / SystemUI
    non-responsiveness, while staying closer to battery-first than stock.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -101,9 +101,9 @@ DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 uint __read_mostly sched_bore                   = 1;
 uint __read_mostly sched_burst_smoothness_long  = 1;
 uint __read_mostly sched_burst_smoothness_short = 1;
-uint __read_mostly sched_burst_fork_atavistic   = 2;
-uint __read_mostly sched_burst_penalty_offset   = 18;
-uint __read_mostly sched_burst_penalty_scale    = 1536;
+uint __read_mostly sched_burst_fork_atavistic   = 1;
+uint __read_mostly sched_burst_penalty_offset   = 20;
+uint __read_mostly sched_burst_penalty_scale    = 1408;
 uint __read_mostly sched_burst_cache_lifetime   = 90000000;
 
 #define MAX_BURST_PENALTY (39U <<2)
